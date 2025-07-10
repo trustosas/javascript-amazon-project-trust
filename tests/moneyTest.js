a79d45eb00322e698146df5fa2b8fe2b732e444f
trustosas <osayandeosas1000@gmail.com>
2025-07-10 14:48:24 +0100

    add automated tests for formatCurrency

---
+++ tests/moneyTest.js
@@ -0,0 +1,23 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+
+function test(func, res, ...args) {
+  if (func(...args) === res) {
+    console.log('passed');
+  } else {
+    console.log('failed');
+  }
+}
+
+console.log('test suite: formatCurrency');
+
+console.log('convert cents');
+test(formatCurrency, "20.95", 2095);
+
+console.log('works with 0');
+test(formatCurrency, "0.00", 0);
+
+console.log('rounds up to nearest cent');
+test(formatCurrency, "20.01", 2000.5);
+
+console.log('rounds down to nearest cent');
+test(formatCurrency, "20.00", 2000.4);
