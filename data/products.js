451fa23fd5b5ecaa5b9b17244b60c93737a9faa5
trustosas <osayandeosas1000@gmail.com>
2025-07-16 08:14:51 +0100

    convert products into classes

--- data/products.js
+++ data/products.js
@@ -1,3 +1,5 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+
 export function getProduct(productId) {
   let matchingProduct;
   products.forEach((product) => {
@@ -7,6 +9,31 @@ export function getProduct(productId) {
   });
   
   return matchingProduct;
+}
+
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+  
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+  
+  getStarsUrl() {
+    return `images/ratings/rating-${this.rating.stars * 10}.png`
+  }
+  
+  getPrice() {
+    return `$${formatCurrency(this.priceCents)}`
+  }
+  
 }
 
 export const products = [
@@ -700,4 +727,4 @@ export const products = [
      "lightweight"
    ]
  }
-];
+].map((productDetails) => {return new Product(productDetails)});
